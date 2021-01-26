789d6472c155f787dbc805b69795d5a4dc179f99
SmitaRothe <67286874+SmitaRothe@users.noreply.github.com>
2021-01-26 12:08:36 -0500

    added some methods in rentalDialog.cs for clearinputs, IsFieldsValid,  returnDateSelectionChange, cmbCars_SelectionChanged and also added try and catch blocks for error handling..

--- CarRental/CarRental/RentalDialog.xaml.cs
+++ CarRental/CarRental/RentalDialog.xaml.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using Microsoft.Win32;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -20,6 +22,7 @@ namespace CarRental
     public partial class RentalDialog : Window
     {
         Customer currCustomer;
+        byte[] currCarImage;
         Car currCar;
 
         public RentalDialog(Customer customer)
@@ -45,20 +48,29 @@ namespace CarRental
         // Save Rental
         private void btnSaveRental_Click(object sender, RoutedEventArgs e)
         {
-            currCar = (Car)cmbCars.SelectedItem;
-
-            Rental rental = new Rental
+            if (!IsFieldsValid()) { return; }
+            try
             {
-                CarId = currCar.CarId,
-                CustomerId = currCustomer.CustomerId,
-                RentalDate = dpRentalDate.SelectedDate.Value,
-                ReturnDate = dpReturnDate.SelectedDate.Value
-            };
+                currCar = (Car)cmbCars.SelectedItem;
 
-            Global.context.Rentals.Add(rental);
-            Global.context.SaveChanges();
+                Rental rental = new Rental
+                {
+                    CarId = currCar.CarId,
+                    CustomerId = currCustomer.CustomerId,
+                    RentalDate = dpRentalDate.SelectedDate.Value,
+                    ReturnDate = dpReturnDate.SelectedDate.Value,
+                    TotalFee = float.Parse(lblTotalFess.Content.ToString())
+                };
 
-            FetchRecord();
+                Global.context.Rentals.Add(rental);
+                Global.context.SaveChanges();
+                ClearInputs();
+                FetchRecord();
+            }
+            catch (SystemException exc)
+            {
+                MessageBox.Show(exc.Message);
+            }
         }
 
         private void LvCarsOnRent_SelectionChanged(object sender, SelectionChangedEventArgs e)
@@ -76,7 +88,68 @@ namespace CarRental
         private void Window_Activated(object sender, EventArgs e)
         {
             FetchRecord();
-
         }
+
+        private void cmbCars_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+            currCar = (Car)cmbCars.SelectedItem;
+            lblRentalFee.Content = currCar.RentalFee;
+            currCarImage = currCar.Photo;
+            BitmapImage bitmap = Utils.ByteArrayToBitmapImage(currCarImage); // ex: SystemException
+            imageViewer.Source = bitmap;
+            
+        }
+
+       
+        private void returnDateSelectionChange(object sender, SelectionChangedEventArgs e)
+        {
+
+            currCar = (Car)cmbCars.SelectedItem;
+            int NumOfDays;
+            
+            if (dpRentalDate.SelectedDate < dpReturnDate.SelectedDate)
+            {
+                NumOfDays = (dpReturnDate.SelectedDate - dpRentalDate.SelectedDate).Value.Days;
+                lblTotalFess.Content = (NumOfDays * currCar.RentalFee).ToString();
+            }
+            else
+            {              
+                MessageBox.Show("The date selected must be more than starting date");
+                dpReturnDate.SelectedDate = null;
+                return;
+            }
+        }
+
+        public bool IsFieldsValid()
+        {
+            if (lblCustomerName.Content.ToString() == "" || cmbCars.Text == "" || lblRentalFee.Content.ToString() == "" || dpRentalDate.Text == "" || dpReturnDate.Text == "" ||
+                 lblTotalFess.Content.ToString()== "")
+            {
+                MessageBox.Show("All fields must be filled", "Validation error", MessageBoxButton.OK, MessageBoxImage.Warning);
+                return false;
+            }
+            if (currCarImage == null)
+            {
+                MessageBox.Show("Please choose a picture", "Validation error", MessageBoxButton.OK, MessageBoxImage.Warning);
+                return false;
+            }
+            return true;
+        }
+
+        public void ClearInputs()
+        {
+            btnChooseClient.Content = "";
+            lblCustomerName.Content = "";
+            cmbCars.Text = "";
+            lblRentalFee.Content = "";
+            dpRentalDate.Text = "";
+            dpReturnDate.Text = "";
+            lblTotalFess.Content = "";
+            btnUpdateRental.IsEnabled = false;
+            btnClose.IsEnabled = false;
+            imageViewer.Source = null;
+        }
+
     }
 }
